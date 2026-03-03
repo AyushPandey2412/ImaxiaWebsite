7d092049feafcbaecdf00da1001ad5876f3af21e
AyushPandey2412 <ayushpandey5248@gmail.com>
2026-03-03 19:10:14 +0530

    solved problem in single item

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,16 +1,29 @@
+// import Home from "@/components/Home";
+// import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "NextCommerce | Nextjs E-commerce template",
+//   description: "This is Home for NextCommerce Template",
+//   // other metadata
+// };
+
+// export default function HomePage() {
+//   return (
+//     <>
+//       <Home />
+//     </>
+//   );
+// }
+
+
 import Home from "@/components/Home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextCommerce | Nextjs E-commerce template",
-  description: "This is Home for NextCommerce Template",
-  // other metadata
+  title: "Imaxia Professional",
+  description: "Welcome to Imaxia Professional - Premium Beauty & Skincare Products",
 };
 
 export default function HomePage() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <Home />;
 }
