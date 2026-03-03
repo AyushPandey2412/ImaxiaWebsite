0f3eb1a5f357772267615191ac6097251f3f4107
AyushPandey2412 <ayushpandey5248@gmail.com>
2026-03-03 18:34:25 +0530

    solved build error

--- src/redux/features/quickView-slice.ts
+++ src/redux/features/quickView-slice.ts
@@ -1,3 +1,47 @@
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { Product } from "@/types/product";
+
+// type InitialState = {
+//   value: Product;
+// };
+
+// const initialState = {
+//   value: {
+//     title: "",
+//     reviews: 0,
+//     price: 0,
+//     discountedPrice: 0,
+//     img: "",
+//     id: 0,
+//     images: [],
+//     imgs: { thumbnails: [], previews: [] },
+//   } as Product,
+// } as InitialState;
+
+// export const quickView = createSlice({
+//   name: "quickView",
+//   initialState,
+//   reducers: {
+//     updateQuickView: (_, action) => {
+//       return {
+//         value: {
+//           ...action.payload,
+//         },
+//       };
+//     },
+
+//     resetQuickView: () => {
+//       return {
+//         value: initialState.value,
+//       };
+//     },
+//   },
+// });
+
+// export const { updateQuickView, resetQuickView } = quickView.actions;
+// export default quickView.reducer;
+
+
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Product } from "@/types/product";
 
@@ -5,35 +49,41 @@ type InitialState = {
   value: Product;
 };
 
-const initialState = {
+const initialState: InitialState = {
   value: {
+    id: 0,
     title: "",
+    description: "",
+    category: "",
     reviews: 0,
     price: 0,
     discountedPrice: 0,
-    img: "",
-    id: 0,
-    images: [],
-    imgs: { thumbnails: [], previews: [] },
-  } as Product,
-} as InitialState;
+    imgs: {
+      thumbnails: [],
+      previews: [],
+    },
+
+    // optional fields
+    longDescription: "",
+    keyBenefits: [],
+    keyIngredients: [],
+    availableSizes: [],
+    suitableFor: "",
+    usage: "",
+    tag: "",
+  },
+};
 
 export const quickView = createSlice({
   name: "quickView",
   initialState,
   reducers: {
-    updateQuickView: (_, action) => {
-      return {
-        value: {
-          ...action.payload,
-        },
-      };
+    updateQuickView: (state, action: PayloadAction<Product>) => {
+      state.value = action.payload;
     },
 
-    resetQuickView: () => {
-      return {
-        value: initialState.value,
-      };
+    resetQuickView: (state) => {
+      state.value = initialState.value;
     },
   },
 });
